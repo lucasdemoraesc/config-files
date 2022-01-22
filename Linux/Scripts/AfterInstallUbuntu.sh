72bec91172661b62fc3b36fcf34aff1708733de4
Lucas Corrêa <lucas.correa@maximatech.com.br>
2022-01-22 17:31:48 -0300

    Adiciona script de pós instalação

---
+++ Linux/Scripts/AfterInstallUbuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script para configuração do Ubuntu
+
+AtualizarPacotes() {
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get install -f
+	sudo dpkg --configure -a
+}
+
+RemoverProgramasInuteis() {
+
+}
+
+InstalarProgramasEssenciais() {
+	echo "🛠️ Instalando alguns utilitários"
+	sleep 1s
+	sudo apt install neofetch nautilus-actions filemanager-actions filemanager-actions-doc flameshot grub-customizer gparted vim gnome-tweaks deborphan
+}
