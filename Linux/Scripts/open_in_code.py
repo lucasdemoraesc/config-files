40a871bbd43c31a270e3756d763ae6cdc121a926
Lucas Moraes <lucas.correa@maximatech.com.br>
2025-07-03 12:20:07 -0300

    Adiciona script open in code

---
+++ Linux/Scripts/open_in_code.py
@@ -0,0 +1,67 @@
+# Extensão para abrir diretórios no VS Code através do menu de contexto do Nautilus
+import os
+from typing import List
+from urllib.parse import unquote
+
+from gi.repository import GObject, Nautilus
+
+
+class OpenCodeExtension(GObject.GObject, Nautilus.MenuProvider):
+    def _open_code(self, file: Nautilus.FileInfo) -> None:
+        filename = unquote(file.get_uri()[7:])
+
+        # Abre o diretório no VS Code
+        os.system(f"code '{filename}'")
+
+    def menu_activate_cb(
+        self,
+        menu: Nautilus.MenuItem,
+        file: Nautilus.FileInfo,
+    ) -> None:
+        self._open_code(file)
+
+    def menu_background_activate_cb(
+        self,
+        menu: Nautilus.MenuItem,
+        file: Nautilus.FileInfo,
+    ) -> None:
+        self._open_code(file)
+
+    def get_file_items(
+        self,
+        files: List[Nautilus.FileInfo],
+    ) -> List[Nautilus.MenuItem]:
+        if len(files) != 1:
+            return []
+
+        file = files[0]
+        if file.get_uri_scheme() != "file":
+            return []
+
+        # Funciona tanto para diretórios quanto para arquivos
+        item = Nautilus.MenuItem(
+            name="NautilusPython::opencode_file_item",
+            label="Abrir no VS Code",
+            tip="Abrir %s no VS Code" % file.get_name(),
+            icon="code",  # Ícone do VS Code
+        )
+        item.connect("activate", self.menu_activate_cb, file)
+
+        return [
+            item,
+        ]
+
+    def get_background_items(
+        self,
+        current_folder: Nautilus.FileInfo,
+    ) -> List[Nautilus.MenuItem]:
+        item = Nautilus.MenuItem(
+            name="NautilusPython::opencode_background_item",
+            label="Abrir no VS Code",
+            tip="Abrir %s no VS Code" % current_folder.get_name(),
+        )
+        item.connect("activate", self.menu_background_activate_cb, current_folder)
+
+        return [
+            item,
+        ]
